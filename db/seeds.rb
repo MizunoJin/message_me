a03fb6d092a4b059f1205f36923bd9f1ad6b8009
fantagreep <bump.lk_k.1215@icloud.com>
2021-08-14 22:11:46 +0900

    Create message table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Jan", password: "password")
+User.create(name: "Jin", password: "password")
+User.create(name: "Jun", password: "password")
+User.create(name: "Jen", password: "password")
+User.create(name: "Jon", password: "password")
